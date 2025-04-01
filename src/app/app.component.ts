1ac0729b14d4fc4fbd90ea059c9566b59290d9d4
vivek taware <vivektaware16@gmail.com>
2025-04-01 20:17:32 +0530

    first comment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { NavComponent } from "./component/nav/nav.component";
+import { HeroComponent } from "./component/hero/hero.component";
+import { AboutpageComponent } from "./component/aboutpage/aboutpage.component";
+import { FooterComponent } from "./component/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavComponent, RouterLink, HeroComponent, AboutpageComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
